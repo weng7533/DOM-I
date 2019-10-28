560239c834fc651be284048dd68a5c83db96a8c6
weng7533 <weng7533@users.noreply.github.com>
2019-10-28 18:30:24 -0400

    all done, sorry forgot to commit today...

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+
+
 const siteContent = {
   "nav": {
     "nav-item-1": "Services",
@@ -40,3 +43,106 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const header = document.querySelector('header');
+const a1 = document.querySelector('a:nth-child(1)');
+const a2 = document.querySelector('a:nth-child(2)');
+const a3 = document.querySelector('a:nth-child(3)');
+const a4 = document.querySelector('a:nth-child(4)');
+const a5 = document.querySelector('a:nth-child(5)');
+const a6 = document.querySelector('a:nth-child(6)');
+
+a1.textContent = siteContent.nav["nav-item-1"];
+a2.textContent = siteContent.nav["nav-item-2"];
+a3.textContent = siteContent.nav["nav-item-3"];
+a4.textContent = siteContent.nav["nav-item-4"];
+a5.textContent = siteContent.nav["nav-item-5"];
+a6.textContent = siteContent.nav["nav-item-6"];
+
+
+
+
+const ctaCtaTextH1 = document.querySelector('.cta .cta-text h1');
+//console.log(ctaCtaTextH1)
+ctaCtaTextH1.textContent = siteContent.cta["h1"];
+ctaCtaTextH1.style.display = 'flex';
+ctaCtaTextH1.style.wordSpacing = '100vw';
+
+const ctaCtaTextButton = document.querySelector('.cta .cta-text button');
+ctaCtaTextButton.textContent = siteContent.cta["button"];
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = siteContent.cta["img-src"];
+
+const mainContentH41 = document.querySelector('.main-content .top-content .text-content:nth-child(1) h4:nth-child(1)');
+mainContentH41.textContent = siteContent["main-content"]["features-h4"];
+
+const mainContentP1 = document.querySelector('.main-content .top-content .text-content:nth-child(1) p')
+mainContentP1.textContent = siteContent["main-content"]["features-content"];
+
+const mainContentH2 = document.querySelector('.main-content .top-content .text-content:nth-child(2) h4')
+mainContentH2.textContent = siteContent["main-content"]["about-h4"];
+
+const mainContentP2 = document.querySelector('.main-content .top-content .text-content:nth-child(2) p')
+mainContentP2.textContent = siteContent["main-content"]["about-content"];
+
+
+
+
+const mainContentImg = document.querySelector('.main-content img')
+mainContentImg.src = siteContent["main-content"]["middle-img-src"];
+
+
+const bottomContentH41 = document.querySelector('.main-content .bottom-content .text-content:nth-child(1) h4')
+bottomContentH41.textContent = siteContent["main-content"]["services-h4"];
+
+const bottomContentP1 = document.querySelector('.main-content .bottom-content .text-content:nth-child(1) p')
+bottomContentP1.textContent = siteContent["main-content"]["services-content"];
+
+
+const bottomContentH42 = document.querySelector('.main-content .bottom-content .text-content:nth-child(2) h4')
+bottomContentH42.textContent = siteContent["main-content"]["product-h4"];
+
+const bottomContentP2 = document.querySelector('.main-content .bottom-content .text-content:nth-child(2) p')
+bottomContentP2.textContent = siteContent["main-content"]["product-content"];
+
+const bottomContentH43 = document.querySelector('.main-content .bottom-content .text-content:nth-child(3) h4')
+bottomContentH43.textContent = siteContent["main-content"]["vision-h4"];
+
+const bottomContentP3 = document.querySelector('.main-content .bottom-content .text-content:nth-child(3) p')
+bottomContentP3.textContent = siteContent["main-content"]["vision-content"];
+
+
+const contactH4 = document.querySelector('.contact h4')
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+const contactP1 = document.querySelector('.contact p:nth-child(2)');
+contactP1.textContent = siteContent["contact"]["address"];
+
+const contactP2 = document.querySelector('.contact p:nth-child(3)');
+contactP2.textContent = siteContent["contact"]["phone"];
+
+const contactP3 = document.querySelector('.contact p:nth-child(4)');
+contactP3.textContent = siteContent["contact"]["email"];
+
+const footerP = document.querySelector('footer p');
+footerP.textContent = siteContent['footer']['copyright'];
+
+document.querySelectorAll('header nav a').forEach(
+  function (element){
+    element.style.color = 'green';
+  }
+)
+
+const newItem1 = document.createElement('a')
+newItem1.textContent = 'item1';
+const newItem2 = document.createElement('a')
+newItem2.textContent = 'item2'
+
+document.querySelector('header nav').prepend(newItem1);
+document.querySelector('header nav').appendChild(newItem2);
+
+header.style.border = 'thick  dotted red';
+
+
+console.log(header.style)
